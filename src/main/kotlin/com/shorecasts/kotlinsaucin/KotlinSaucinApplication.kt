0339a48bf159c33544c5c7cf8b1192e95a99f402
Chase Gaddis <cgaddis36@gmail.com>
2023-05-21 23:32:33 -0400

    Initial Build

---
+++ src/main/kotlin/com/shorecasts/kotlinsaucin/KotlinSaucinApplication.kt
@@ -0,0 +1,11 @@
+package com.shorecasts.kotlinsaucin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSaucinApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSaucinApplication>(*args)
+}
---
+++ src/test/kotlin/com/shorecasts/kotlinsaucin/KotlinSaucinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shorecasts.kotlinsaucin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSaucinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
